efa066fa2dd5d0ebe35ea953867e2435afd5ec92
jerobe <jerobejrobinson@gmail.com>
2021-07-02 19:59:35 -0500

    fixing ServiceCard padding and size

--- comps/ServiceCard.js
+++ comps/ServiceCard.js
@@ -3,11 +3,11 @@ import Link from 'next/link'
 
 const ServiceCard = ({service, hours, price, desc, imgSrc, url, w, h}) => {
     return (
-        <div className="mt-10 w-80 mx-auto bg-gray-100">
+        <div className="mt-10 w-11/12 sm:w-80 mx-auto bg-gray-100">
             <div>
                 <Image src={imgSrc} width={w} height={h}/>
             </div>
-            <div className="px-2 mt-6 pb-4">
+            <div className="px-5 mt-6 pb-4">
                 <div className="flex justify-between">
                     <h2 className="font-bold">{service}</h2>
                     <span className="font-light">{hours} {price}</span>
