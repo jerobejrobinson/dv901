51e2415735a17b485f7a4e4cbb63815a9e0166b5
jerobe <jerobejrobinson@gmail.com>
2021-07-02 19:46:35 -0500

    fixing &apos; eslint errors

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
           <h1 className="font-bold text-3xl">Dreamy Vibes 901</h1>
           <p className="uppercase">Where your lash dreams come true!</p>
         </div>
-        <p className="p-5 mt-12">Hello, my dreamers!<br/>My name is Angelica Holmes and I'm a lash tech based in Memphis, Tn Here you can fulfil your lash dreams, either it's a <strong>classic</strong>, <strong>hybrid</strong>, or <strong>volume</strong> set. I can fulfil that for you! </p>
+        <p className="p-5 mt-12">Hello, my dreamers!<br/>My name is Angelica Holmes and I&apos;m a lash tech based in Memphis, Tn Here you can fulfil your lash dreams, either it&apos;s a <strong>classic</strong>, <strong>hybrid</strong>, or <strong>volume</strong> set. I can fulfil that for you! </p>
         <button className="bg-gray-50	p-5 mt-12">
           Book A Session Now
         </button>
